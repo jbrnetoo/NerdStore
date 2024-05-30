6bcc0b696c9855dbb3f2870cd87bd1786238b86e
Joao Neto <joaoneto.rocha20@gmail.com>
2024-05-30 20:58:41 -0300

    feat: criação da classe Dimensoes; validações das entidades; criação de exceção especifica para erros de dominio

--- src/NerdStore.Catalago.Domain/Produto.cs
+++ src/NerdStore.Catalago.Domain/Produto.cs
@@ -12,9 +12,17 @@ namespace NerdStore.Catalago.Domain
         public DateTime DataCadastro { get; private set; }
         public string Imagem { get; private set; }
         public int QuantidadeEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
         public Categoria Categoria { get; private set; }
 
-        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        public Produto(string nome,
+                       string descricao,
+                       bool ativo,
+                       decimal valor,
+                       Guid categoriaId,
+                       DateTime dataCadastro,
+                       string imagem,
+                       Dimensoes dimensoes)
         {
             CategoriaId = categoriaId;
             Nome = nome;
@@ -23,6 +31,9 @@ namespace NerdStore.Catalago.Domain
             Valor = valor;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;
+
+            Validar();
         }
 
         // Ad-hoc setter
@@ -39,12 +50,14 @@ namespace NerdStore.Catalago.Domain
 
         public void AlterarDescricao(string descricao)
         {
+            Validacoes.ValidarSeVazio(descricao, "O campo Descricao do produto não pode estar vazio");
             Descricao = descricao;
         }
 
         public void DebitarEstoque(int quantidade)
         {
             if (quantidade < 0) quantidade *= -1;
+            if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
             QuantidadeEstoque -= quantidade; 
         }
 
@@ -60,25 +73,11 @@ namespace NerdStore.Catalago.Domain
 
         public void Validar()
         {
-            // TODO
-        }
-    }
-
-    public class Categoria : Entity
-    {
-        public string Nome { get; private set; }
-
-        public int Codigo { get; private set; }
-
-        public Categoria(string nome, int codigo)
-        {
-            Nome = nome;
-            Codigo = codigo;
-        }
-
-        public override string ToString()
-        {
-            return $"{Nome} - {Codigo}";
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+            Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
         }
     }
 }
