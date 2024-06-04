17b43d4a81de4b4f4338f55875692430d5d78a21
João Batista da Rocha Neto <framework25@localiza.com>
2024-06-04 16:55:25 -0300

    feat: criação de classes de viewModel; criação do CatalogoProfile

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/CatalogoProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class CatalogoProfile : Profile
+    {
+        public CatalogoProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+               .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+               .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+               .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+
+            CreateMap<ProdutoViewModel, Produto>()
+               .ConstructUsing(p =>
+                   new Produto(p.Nome, p.Descricao, p.Ativo,
+                       p.Valor, p.CategoriaId, p.DataCadastro,
+                       p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
