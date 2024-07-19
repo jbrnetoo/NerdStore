ea1966d68bd32bafbb826a1b324ae6649e8fb5bb
Joao Neto <joaoneto.rocha20@gmail.com>
2024-07-18 21:15:04 -0300

    feat: organização dos diretórios do Core; Criação de novos métodos

--- src/NerdStore.WebApp.MVC/Controllers/BaseController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/BaseController.cs
@@ -1,10 +1,38 @@
 ﻿
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 
 namespace NerdStore.WebApp.MVC.Controllers
 {
     public abstract class BaseController : Controller
     {
+        private readonly DomainNotificationHandler _notifications;
+        private readonly IMediatorHandler _mediatorHandler;
+
         protected Guid ClienteId = Guid.Parse("c38ea4df-0b95-4b53-b9e3-bf81ac3d2a7a");
+
+        protected BaseController(INotificationHandler<DomainNotification> notifications,
+                                 IMediatorHandler mediatorHandler)
+        {
+            _notifications = (DomainNotificationHandler)notifications;
+            _mediatorHandler = mediatorHandler;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notifications.TemNotificacao();
+        }
+
+        protected IEnumerable<string> ObterMensagensErro()
+        {
+            return _notifications.ObterNotificacoes().Select(c => c.Value).ToList();
+        }
+
+        protected void NotificarErro(string codigo, string mensagem)
+        {
+            _mediatorHandler.PublicarNotificacao(new DomainNotification(codigo, mensagem));
+        }
     }
 }
